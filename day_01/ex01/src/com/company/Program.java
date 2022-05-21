5d566385bdc1527dfcec70ac46838fb9c5b558bf
Elasticnoise <vs.tretyakov01@gmail.com>
2022-05-21 17:20:35 +0300

    ex01 - done

---
+++ day_01/ex01/src/com/company/Program.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Program {
+
+    public static void main(String[] args) {
+        User first = new User("Bot", 100);
+        User second = new User("Bot2", 200);
+
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
